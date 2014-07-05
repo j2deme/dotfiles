e63b278f10e8c2a1f3b68ed48f65b01683e249f3
Jaime Delgado Meraz <j2deme@gmail.com>
2014-07-05 11:41:22 -0500

    Added minecraft launcher

---
+++ minecraft.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#java -Xmx1024M -Xms512M -cp /home/j2deme/.minecraft/Minecraft.jar net.minecraft.bootstrap.Bootstrap
+java -Xmx1024M -Xms512M -cp /home/j2deme/minecraft/PreLauncherESP2.1.jar net.minecraft.bootstrap.Bootstrap
